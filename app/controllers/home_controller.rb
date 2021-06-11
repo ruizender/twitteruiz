8d207fa2ef0f9d1bd1f61966dc6c8a180910f1b3
Ender Ruiz <ruiz.valera@gmail.com>
2021-06-11 09:59:47 -0400

    modify controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!
+    skip_before_action :authenticate_user!
   def index
     @tweets = Tweet.order("created_at DESC")
     @tweets = Kaminari.paginate_array(@tweets).page(params[:page]).per(50)
