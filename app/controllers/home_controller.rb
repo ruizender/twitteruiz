2b66f7e5ff25d5debc1738584120ecf08ffc2fd9
Ender Ruiz <ruiz.valera@gmail.com>
2021-06-20 20:05:25 -0400

    add funtion hashtag

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,15 +2,19 @@ class HomeController < ApplicationController
     skip_before_action :authenticate_user!
   def index
     
-    if user_signed_in?
+    if signed_in?
       @tweets = Tweet.tweets_for_me(current_user).order("created_at DESC")
-    elsif params[:q].present?
-        @tweets = Tweet.where('textweet LIKE ?', "%#{params[:q]}%")
+      if params[:q].present?
+        @tweets = Tweet.tweets_for_me(current_user).order("created_at DESC").where('textweet LIKE ?', "%#{params[:q]}%")
+      end
     else
       @tweets = Tweet.all.order("created_at DESC")
+      if params[:q].present?
+        @tweets = Tweet.where('textweet LIKE ?', "%#{params[:q]}%")
+      end
     end
     
-#queda pendiente arreglar problema de que no funciona el buscador
+
     @tweets = Kaminari.paginate_array(@tweets).page(params[:page]).per(50)
     @tweet = Tweet.new
     @like = Like.new
